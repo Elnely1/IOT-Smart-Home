6d33c8cc910d6932d8cf7ca686602849468c2865
Elnely1 <elnelyahmed406@gmail.com>
2024-05-13 04:00:25 +0300

    Comment channel 4

--- Full_Code.cpp
+++ Full_Code.cpp
@@ -1,20 +1,20 @@
-#include <WiFi.h>                                                                       // This library contains the Wifi function to run on ESP-32
+#include <WiFi.h>                                                                                     // This library contains the Wifi function to run on ESP-32
 #include <ESP32Servo.h>
-                                                                                      // In these 4 lines we have defined the PWM details for LED
+                                                                                                     // In these 4 lines we have defined the PWM details for LED
 #define LEDC_TIMER_13_BIT  13
 #define LEDC_BASE_FREQ     5000
 #define LED_PIN            12
 #define LED_PIN1           14
 #define LED_PIN2           15
 #define LED_PIN3           27
-#define LED_PIN4           4
+// #define LED_PIN4           4
 #define SERVO_PIN          13
 #define LEDC_CHANNEL_0     0
 #define Led_channel_1      1
 #define Led_channel_2      2
 #define Led_channel_3      3
-#define Led_channel_4      4
-#define min(a,b) ((a)<(b)?(a):(b));                                                     // Defining Min Function
+// #define Led_channel_4      4
+#define min(a,b) ((a)<(b)?(a):(b));                                                                    // Defining Min Function
 
 const char* ssid     = "ELNELY";
 const char* password = "1234567890";
@@ -24,7 +24,7 @@ Servo myservo;
 
 int brightness = 0;
 int servoPosition = 0;
-void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)          // function for analogWrite in Arduino 
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)                         // function for analogWrite in Arduino 
 {
   uint32_t duty = (8191 / valueMax) * min(value, valueMax);
   ledcWrite(channel, duty);
@@ -32,7 +32,7 @@ void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255)  
 
 void setup()
 {
-    Serial.begin(115200);                                                               //Baud Rate of ESP-32
+    Serial.begin(115200);                                                                               //Baud Rate of ESP-32
     delay(10);
     Serial.println();
     Serial.print("Connecting to ");
@@ -47,113 +47,113 @@ void setup()
     Serial.println("WiFi connected.");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
-    server.begin();                                                                     // Start the server
-    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                       // PWM setup
+    server.begin();                                                                                     // Start the server
+    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                                       // PWM setup
     ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
     myservo.attach(SERVO_PIN);
-    ledcSetup(Led_channel_1, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                       // PWM setup
+    ledcSetup(Led_channel_1, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                                         // PWM setup
     ledcAttachPin(LED_PIN1, Led_channel_1);
-    ledcSetup(Led_channel_2, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                       // PWM setup
+    ledcSetup(Led_channel_2, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                                         // PWM setup
     ledcAttachPin(LED_PIN2, Led_channel_2);
-    ledcSetup(Led_channel_3, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                       // PWM setup
-    ledcAttachPin(LED_PIN3, Led_channel_3);
-    ledcSetup(Led_channel_4, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                       // PWM setup
-    ledcAttachPin(LED_PIN4, Led_channel_4);
+    ledcSetup(Led_channel_3, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                                         // PWM setup
+    ledcAttachPin(LED_PIN3, Led_channel_3);  
+    // ledcSetup(Led_channel_4, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);                                      // PWM setup
+    // ledcAttachPin(LED_PIN4, Led_channel_4);
       
 }
 int value = 0;
 void loop(){
- WiFiClient client = server.available();                                                // listen for any incoming client
+ WiFiClient client = server.available();                                                                  // listen for any incoming client
  
-  if (client) {                                                                         // if you get any client,
-    Serial.println("New Client.");                                                      // print a message on the serial monitor
-    String currentLine = "";                                                            // make a String to hold incoming data from the client
-    while (client.connected())                                                          // loop while the client's connected
+  if (client) {                                                                                           // if you get any client,
+    Serial.println("New Client.");                                                                        // print a message on the serial monitor
+    String currentLine = "";                                                                              // make a String to hold incoming data from the client
+    while (client.connected())                                                                            // loop while the client's connected
     {                                                        
-      if (client.available())                                                           // if there's bytes to read from the client
+      if (client.available())                                                                             // if there's bytes to read from the client
       {                                                         
-        char c = client.read();                                                         // read a byte, then
-        Serial.write(c);                                                                // print it out on the serial monitor
-        if (c == '\n')                                                                  // if the byte is a newline character
+        char c = client.read();                                                                           // read a byte, then
+        Serial.write(c);                                                                                  // print it out on the serial monitor
+        if (c == '\n')                                                                                    // if the byte is a newline character
         {                                                                
-          if (currentLine.length() == 0)                                                // if the current line is blank, you got two newline characters in a row.
-                                                                                        // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0)                                                                  // if the current line is blank, you got two newline characters in a row.
+                                                                                                          // that's the end of the client HTTP request, so send a response:
           { 
-            client.println("HTTP/1.1 200 OK");                                          // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-            client.println("Content-type:text/html");                                   // and a content-type so the client knows what's coming,
-            client.println();                                                           // then a blank line:
-                                                                                        // the content of the HTTP response follows the header:
-            client.print("<a href=\"/A\">Outdoor ON</a><br>");
-            client.print("<a href=\"/B\">Outdoor OFF</a><br>");
-            client.print("<a href=\"/C\">DOOR ON</a><br>");
-            client.print("<a href=\"/D\">DOOR OFF</a><br>");
+            client.println("HTTP/1.1 200 OK");                                                            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            client.println("Content-type:text/html");                                                     // and a content-type so the client knows what's coming,
+            client.println();                                                                             // then a blank line:
+                                                                                                          // the content of the HTTP response follows the header:
+            client.print("<a href=\"/A\">Room ON</a><br>");
+            client.print("<a href=\"/B\">Room OFF</a><br>");
             client.print("<a href=\"/E\">Room1 ON</a><br>");
             client.print("<a href=\"/F\">Room1 OFF</a><br>");
             client.print("<a href=\"/G\">Room2 ON</a><br>");
             client.print("<a href=\"/H\">Room2 OFF</a><br>");
             client.print("<a href=\"/J\">Room3 ON</a><br>");
             client.print("<a href=\"/K\">Room3 OFF</a><br>");
+             client.print("<a href=\"/C\">DOOR ON</a><br>");
+            client.print("<a href=\"/D\">DOOR OFF</a><br>");
             client.print("<a href=\"/I\">OFF All</a><br>");
             // client.print("<a href=\"/J\">Level 5 Full</a><br>");
-            client.println();                                                            // The HTTP response ends with another blank line:
-            break;                                                                       // break out of the while loop: 
+            client.println();                                                                            // The HTTP response ends with another blank line:
+            break;                                                                                       // break out of the while loop: 
           } 
           else 
           {    
-            currentLine = "";                                                            // if you got a newline, then clear currentLine:
+            currentLine = "";                                                                            // if you got a newline, then clear currentLine:
           }
         } 
-        else if (c != '\r')                                                              // if you got anything else but a carriage return character,
+        else if (c != '\r')                                                                              // if you got anything else but a carriage return character,
         {  
-          currentLine += c;                                                              // add it to the end of the currentLine
+          currentLine += c;                                                                              // add it to the end of the currentLine
         }
-        if (currentLine.endsWith("GET /A"))                                              // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+        if (currentLine.endsWith("GET /A"))                    //ROOM on                                 // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(LEDC_CHANNEL_0, 255);   
         }
-        if (currentLine.endsWith("GET /B"))                                              // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+        if (currentLine.endsWith("GET /B"))                    //Room off                                // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(LEDC_CHANNEL_0, 0);   
         }
-        if (currentLine.endsWith("GET /C")) 
+        if (currentLine.endsWith("GET /C"))                    //Door on
         {
           myservo.write(180);
-          ledcAnalogWrite(Led_channel_4, 255);
+          // ledcAnalogWrite(Led_channel_4, 255);
         }
-        if (currentLine.endsWith("GET /D")) 
+        if (currentLine.endsWith("GET /D"))                    //Door off
         {
-          myservo.write(0);
-          ledcAnalogWrite(Led_channel_4, 0);
+          myservo.write(90);
+          // ledcAnalogWrite(Led_channel_4, 0);
         }
-         if (currentLine.endsWith("GET /E"))        //room1 on                                      // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+         if (currentLine.endsWith("GET /E"))                    //Room1 on                              // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(Led_channel_1, 255);   
         }
-        if (currentLine.endsWith("GET /F"))           //room1 off                                   // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+        if (currentLine.endsWith("GET /F"))                     //Room1 off                             // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(Led_channel_1, 0);   
         }
-         if (currentLine.endsWith("GET /G"))             //room2 on                                 // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+         if (currentLine.endsWith("GET /G"))                    //Room2 on                              // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(Led_channel_2, 255);   
         }
-        if (currentLine.endsWith("GET /H"))             //room2 off                                // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+        if (currentLine.endsWith("GET /H"))                     //Room2 off                             // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(Led_channel_2, 0);   
         }
-        if (currentLine.endsWith("GET /J"))             //room3 on                               // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+        if (currentLine.endsWith("GET /J"))                     //Room3 on                              // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(Led_channel_3, 255);   
         }
-        if (currentLine.endsWith("GET /K"))             //room3 off                                // Check to see if the client request was "GET /A" or "GET /B" and so on...:
+        if (currentLine.endsWith("GET /K"))                     //Room3 off                             // Check to see if the client request was "GET /A" or "GET /B" and so on...:
         {
           ledcAnalogWrite(Led_channel_3, 0);   
         }
-        if (currentLine.endsWith("GET /I"))           //off all
+        if (currentLine.endsWith("GET /I"))                     //off all
         {
          ledcAnalogWrite(LEDC_CHANNEL_0, 0);
          myservo.write(0);
-         ledcAnalogWrite(Led_channel_4, 0);
+        //  ledcAnalogWrite(Led_channel_4, 0);
          ledcAnalogWrite(Led_channel_1, 0);
          ledcAnalogWrite(Led_channel_2, 0);
          ledcAnalogWrite(Led_channel_3, 0);
